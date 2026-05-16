8ceb645d2efb84b85f689950e1c6abb67409b092
Natalia Wszelaki <nwszelaki@student.agh.edu.pl>
2026-05-16 11:35:38 +0200

    Reaktywnosc

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useState} from "react";
+
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
+
+
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
     }
+
+    let validationMessage = '';
+        if(email.length < 5) {
+            validationMessage = 'Email jest za krótki';
+        } else if(email.length > 15) {
+            validationMessage = 'Email jest za długi';
+        } else {
+            validationMessage = 'Email jest ok';
+        }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Twój e-mail to {email}</h2>
+            <h3>Twój email wielkimi literami to {email.toUpperCase()} </h3>
+            <input type="text"  onChange={handleChange}/>
+            {email.length > 0 && <div>Błąd: {validationMessage} </div>}
         </div>
     );
 }
